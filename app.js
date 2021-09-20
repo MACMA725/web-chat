a794cc40d43fd038119c6e703b91213a28e59b54
MACAM725 <akhyameme@gmail.com>
2021-09-20 10:31:40 +0700

    chatapp

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-const app = require("express")()
-const server = require("http").createServer(app)
+const app = require("express")();
+const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
-    cors: {
-        origin: 'http://localhost:8080'
-    }
-})
-const expressLayouts = require('express-ejs-layouts')
+  cors: {
+    origin: "http://localhost:8080",
+  },
+});
+const expressLayouts = require("express-ejs-layouts");
 
 // SETUP EJS
-app.set('view engine', 'ejs')
-app.use(expressLayouts)
+app.set("view engine", "ejs");
+app.use(expressLayouts);
 
-app.get('/', (req,res) => {
-    res.render('index', {
-        title: 'CHAT BARENG YUK',
-        layout: 'layout/main'
-    })
-})
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "CHAT BARENG YUK",
+    layout: "layout/main",
+  });
+});
 
-io.on('connection', (socket) => {
-    console.log('coneting')
-    socket.on('disconnect', () => {
-        console.log('disconnect')
-    })
-    socket.on('chat-message', msg => {
-        console.log(`message: ${msg}`)
-    })
-})
+io.on("connection", (socket) => {
+  console.log("coneting");
+  socket.on("disconnect", () => {
+    console.log("disconnect");
+  });
+  socket.on("chat-message", (msg) => {
+    console.log(`message: ${msg}`);
+  });
+});
 
-const port = 8000
+const port = 8000;
 server.listen(port, () => {
-    console.log('listening on localhost:%s', port);
-})
+  console.log("listening on localhost:%s", port);
+});
